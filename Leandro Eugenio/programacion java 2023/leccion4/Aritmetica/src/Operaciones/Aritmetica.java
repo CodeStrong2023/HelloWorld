6181da826736f8668225353db01f2c1f87a35223
Leandro Eugenio <leoeugenio16@gmail.com>
2023-09-14 12:25:11 -0300

    Paso de argumentos en java y operador this

--- Leandro Eugenio/programacion java 2023/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Leandro Eugenio/programacion java 2023/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -9,7 +9,19 @@ public class Aritmetica {
     //Metodo
     public void sumarNumeros(){
         int resultado = a + b;
-        System.out.println("el resultado es = " + resultado)
-                ;
+        System.out.println("el resultado es = " + resultado);
+    }
+    
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return this.a + this.b;
+    }
+    
+    public int sumarConArgumentos(int arg1,int arg2){
+        this.a = a;//El argumento a se adigna al atributo this.a
+        this.b = b;
+        // return a + b;
+        return sumarConRetorno();
+    
     }
 }
