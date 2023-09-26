4cda60617cbb5b39711e828e97626858eab4ec6b
Alan Garrido <alangarrido44@gmail.com>
2023-09-25 21:29:42 -0300

    Agrego nuevas actividades java clase 1

--- Alan/Java2023/Programacion1/Leccion1/HolaMundo/src/HolaMundo.java
+++ Alan/Java2023/Programacion1/Leccion1/HolaMundo/src/HolaMundo.java
@@ -1,7 +1,11 @@
 
+import java.util.Scanner;
+
 public class HolaMundo {
+
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde java");
+        //para comentar una linea(//), para comentar muchas(/* al principio,*/ al final)
+        /*System.out.println("Hola Mundo desde java");
         
         int miVariable = 10;
         System.out.println(miVariable);
@@ -12,5 +16,72 @@ public class HolaMundo {
         System.out.println(miVariableCadena);
         miVariableCadena = "sigamos creciendo en programacion";
         System.out.println(miVariableCadena);
+         
+
+        //Var - Inferencia de tipos java
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        //soutv + tab
+
+        var usuario = "osvaldo";
+        var titulo = "ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario+(a+b));
+
+        //ejercicio: Caracteres especiales con Java
+        var nombre = "natalia";
+        System.out.println("Nueva linea: \n" + nombre); //diagonal inversa y letra n = salto de linea
+        System.out.println("tabulador \t" + nombre); //diagonal inversa y letra t = salto con tabulador
+        System.out.println("\t .:Menu:.");
+        System.out.println("Retroceso: \b\b"+nombre); //diagonal inversa y letra b = borra el caracter anterior
+        System.out.println("comillas simples: \'"+nombre+"\'"); //diagonal inversa y '= 'palabra'
+        System.out.println("comillas dobles: \""+nombre+"\""); ////diagonal inversa y " = "palabra" 
+
+        //Clase Scanner
+        Scanner entrada = new Scanner (System. in);
+        System.out.println("digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el Profeción: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("resultado: "+titulo2+" "+usuario2);
+        
+
+        byte numEnteroByte = 127;
+        System.out.println("numEnteroByte = "+ numEnteroByte);
+        System.out.println("Valor minimo del byte: "+ Byte.MIN_VALUE);
+        System.out.println("Valor maximo del byte: "+ Byte.MAX_VALUE);
+        
+        short numEnteroShort = 32767;
+        System.out.println("numEnteroShort = "+ numEnteroShort);
+        System.out.println("Valor minimo del short: "+ Short.MIN_VALUE);
+        System.out.println("Valor maximo del short: "+ Short.MAX_VALUE);
+        
+        int numEnteroInt = 2147483647;
+        System.out.println("numEnteroInt = "+ numEnteroInt);
+        System.out.println("Valor minimo del int: "+ Integer.MIN_VALUE);
+        System.out.println("Valor maximo del int: "+ Integer.MAX_VALUE);
+        
+        long numEnteroLong = 9223372036854775807L;
+        System.out.println("numEnteroLong = "+ numEnteroLong);
+        System.out.println("Valor minimo del long: "+ Long.MIN_VALUE);
+        System.out.println("Valor maximo del long: "+ Long.MAX_VALUE);
+        
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = "+ numFloat);
+        System.out.println("Valor minimo del float: "+ Float.MIN_VALUE);
+        System.out.println("Valor maximo del float: "+ Float.MAX_VALUE);
+        
+        double numDouble = 1.7976931348623157E308D;
+        System.out.println("numDouble = "+ numDouble);
+        System.out.println("Valor minimo del double: "+ Double.MIN_VALUE);
+        System.out.println("Valor maximo del double: "+ Double.MAX_VALUE);
+        */
     }
 }
