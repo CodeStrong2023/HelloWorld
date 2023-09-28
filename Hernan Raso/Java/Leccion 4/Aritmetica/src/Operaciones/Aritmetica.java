5318ea3bb6ad5c3dce7dc075575669998dc1305b
Hernan <hernanraso@gmail.com>
2023-09-27 22:31:42 -0300

    Paso de argumentos en java

--- Hernan Raso/Java/Leccion 4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Hernan Raso/Java/Leccion 4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -10,6 +10,15 @@ public class Aritmetica {
     public void sumarNumeros(){
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
-    
+    }
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+    }
+    public int sumarConArgumentos(int arg1, int arg2){
+        this.a = arg1; //El argumento a se asigna al atributo this.a
+        this.b = arg2;
+        //return a + b;
+        return this.sumarConRetorno();
     }
 }
