1d786a699b43efcfb3fccb9eae0b910ded108d65
Leandro Eugenio <leoeugenio16@gmail.com>
2023-10-10 10:45:20 -0300

    clase8, java JS

--- Leandro Eugenio/laboratorio java script/leccion2/02-01-ConcatenacionDeCadenas.js
+++ Leandro Eugenio/laboratorio java script/leccion2/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,12 @@ console.log(juntos)
 
 nombre += apellido;//Tercera concatenacion usando el operador simplificado
 console.log(nombre)
+
+//hoy ya no hay que usar var, se utiliza let y const
+
+let nombre2 = "Pedro";
+console.log(nombre2)
+
+const apellido2 = "Eugenio";
+//apellido2 = "Perez" una constante no puede ser modificada
+console.log(apellido2);
