5ade9d1e73276cc85e596c890fb281a34def8ea3
Flor <popiflorencia@yahoo.com.ar>
2023-10-09 23:56:05 -0300

    Clase 8 Parte 1
    Commiteo clase 8 de Python PARTE 1

---
+++ Flor Carballo/JavaScript/Leccion02/Leccion03/03-01-OperadoresJavaScript.UJS.js
@@ -0,0 +1,8 @@
+//Ejercicio para encontrar números pares
+Let parInpar = 10;
+if(parInpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
---
+++ Flor Carballo/Python/Leccion6/Persona.py
@@ -0,0 +1,20 @@
+class Persona: # Creamos una clase
+    def __init__(self, nombre, apellido, edad): # Se lo llama método Init Dunder
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+persona1 = Persona("Paulina", "Generale", 14)
+print(f"El objeto1 de la persona clase: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}")
+
+
+persona2 = Persona("Francisco", "Generale", 7)
+print(f"El objeto2 de la persona clase: {persona2.nombre} {persona2.apellido} Su edad es: {persona2.edad}")
+
+persona1.nombre = "Regina"
+persona1.apellido = "Carballo"
+persona1.edad = 1
+print(f"El objeto 1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}")
+
+# Los atributos son: caracteristicas
+# Los métodos son: el comportamiento que van a tener los objetos (acciones)
