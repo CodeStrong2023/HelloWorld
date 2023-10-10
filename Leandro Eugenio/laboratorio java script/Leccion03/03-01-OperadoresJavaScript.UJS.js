622c36b65b5670903d92d09772e435060813746f
Leandro Eugenio <leoeugenio16@gmail.com>
2023-10-10 07:02:09 -0300

    clase 07 js

---
+++ Leandro Eugenio/laboratorio java script/Leccion03/03-01-OperadoresJavaScript.UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares
+Let parInpar = 10;
+if(parInpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
+
+//Ejercicio: es mayor de edad
+Let edad = 20, adulto = 18;
+if( edad >= adulto ){
+	console.log("Es una persona adulta");
+}
+else{
+	console.log("Es una persona menor de edad")
+}
