6748996c1d9d1d4b13b4ab9e52bab3a939de3915
Flor <popiflorencia@yahoo.com.ar>
2023-10-10 16:31:44 -0300

    Diferencia entre var, let y const clases JavaScript
    Clase 8 de JavaScript

--- Flor Carballo/JavaScript/Leccion03/03-01-OperadoresJavaScript.UJS.js
+++ Flor Carballo/JavaScript/Leccion03/03-01-OperadoresJavaScript.UJS.js
@@ -1,5 +1,5 @@
 //Ejercicio para encontrar números pares
-Let parInpar = 10;
+let parInpar = 10;
 if(parInpar % 2 == 0){
     console.log("Es un número PAR");
 }
@@ -8,10 +8,19 @@ else{
 }
 
 //Ejercicio: es mayor de edad
-Let edad = 20, adulto = 18;
+let edad = 20, adulto = 18;
 if( edad >= adulto ){
 	console.log("Es una persona adulta");
 }
 else{
 	console.log("Es una persona menor de edad")
+}
+//Ejercicio: Dentro de un rango
+let dentroRango = 5; //Aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if( dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Esta dentro del rango establecido")
+}
+else{
+    console.log("Esta fuera del rango establecido")
 }
