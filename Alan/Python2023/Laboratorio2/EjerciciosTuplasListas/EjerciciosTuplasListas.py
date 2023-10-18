8ac615030ab090284fbcd49d91b3dc498b3f81fd
Alan Garrido <alangarrido44@gmail.com>
2023-10-18 19:02:05 -0300

    añado clases

---
+++ Alan/Python2023/Laboratorio2/EjerciciosTuplasListas/EjerciciosTuplasListas.py
@@ -0,0 +1,9 @@
+#dada la siguiente tupla
+tupla = (13, 1, 8, 3, 2, 5, 8) #definir la tupla
+#crear una lista que solo incluya numeros menores a 5
+#e imprima por consola [1, 3, 2]
+lista = []
+for elemento in tupla:
+    if elemento <5:
+        lista.append(elemento)
+print(lista)
