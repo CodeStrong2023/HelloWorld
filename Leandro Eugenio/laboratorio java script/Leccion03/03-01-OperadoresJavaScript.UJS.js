e2ecbe4d7940ef9f775912c648917a0c591c21a3
Leandro Eugenio <leoeugenio16@gmail.com>
2023-10-25 07:42:05 -0300

    clase 9, algunos Operadres y funciones,js

--- Leandro Eugenio/laboratorio java script/Leccion03/03-01-OperadoresJavaScript.UJS.js
+++ Leandro Eugenio/laboratorio java script/Leccion03/03-01-OperadoresJavaScript.UJS.js
@@ -25,4 +25,38 @@ if(dentroRango >= valMin && dentroRango <= valMax){
     console.log("Esta dentor del rango establecido")
 }
 
+//Ejercicio si el padre puede asistir al juego de su hijo
+let vacaciones = false,diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir")}
+else{
+    console.log('el padre no puede asistir')
+
+}
+
+//Operador ternario
+let resultado = 3 > 2 ? "VERDADERO" : "FALSO";
+
+console.log(resultado);
+let numero = 8;
+resultado= numero % 2 == 0 ? "Par" : "Impar";
+
+//Convertir string a numero
+let miNumero = "10"
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);
+console.log(typeof edad2);
+if(isNaN(edad2)){
+    console.log("Esta variable no contiene numeros")
+}
+
+if(edad2 >= 18){
+    console.log("Puede votar")
+}else{
+    console.log("No puede votar")
+}
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "No puede votar";
+console.log(resultado3)
  
+//convertir string a numbre
