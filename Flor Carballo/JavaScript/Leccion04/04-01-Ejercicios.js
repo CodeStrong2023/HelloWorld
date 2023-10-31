54de1a978c6b2f14e61629079d98446a4b1b73a6
Flor <popiflorencia@yahoo.com.ar>
2023-10-31 13:28:57 -0300

    Ejercicios JavaScript
    Commiteo la Clase 10 de JavaScript: Ejercicios Estación del año y hora del día.

--- Flor Carballo/JavaScript/Leccion04/04-01-Ejercicios.js
+++ Flor Carballo/JavaScript/Leccion04/04-01-Ejercicios.js
@@ -46,3 +46,62 @@ console.log(fechaNacimiento);
 //fechaNacimiento = 2003;
 //console.log(fechaNacimiento); //solo se ejecuta el console anterior
 
+//Ejercicio 1:  Calcular estación del año
+let mes = 1;
+let estacion; 
+if(mes == 1 || mes == 2 || mes == 12){
+    estacion = "Verano";
+}
+else if(mes == 3 || mes == 4 || mes == 5){
+    estacion = "Otoño";
+}
+else if(mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno";
+}
+else if(mes == 9 || mes == 10 || mes == 11){
+    estacion = "Primavera";
+}
+else{
+    estacion = "Valor incorrecto";
+}
+console.log("El valor corresponde a la estación de: "+estacion);
+
+//Ejercicio 2: Hora del día
+let horaDia = 9;
+let mensaje;
+if(horaDia >= 6 && horaDia <= 11){
+    mensaje = "Good Morning";
+}
+else if(horaDia >= 12 && horaDia <= 16){
+    mensaje = "Good afternoon";
+}
+else if(horaDia >= 17 && horaDia <= 19){
+    mensaje = "Good evening";
+}
+else if(horaDia >= 20 && horaDia <= 23){
+    mensaje = "Good night";
+}
+else{
+    mensaje = "Valor incorrecto";
+}
+console.log(mensaje);
+
+//Estructura switch(la sintaxis es igual a Java) EJERCICIO 1:
+switch(mes){ //No solo se pueden utilizar números, también cadenas
+    case 1: case 2: case 12:
+        estacion = "Verano";
+        break;
+    case 3: case 4: case 5:
+        estacion = "Otoño";
+        break;
+    case 6: case 7: case 8:
+        estacion = "Invierno";
+        break;
+    case 9: case 10: case 11:
+        estacion = "Primavera";
+        break;
+    default:
+        estacion = "Valor incorrecto";
+}
+console.log("Bienvenido a la estación de: "+estacion);
+
