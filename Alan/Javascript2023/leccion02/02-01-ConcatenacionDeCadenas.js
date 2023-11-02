6258e1796a2123ad5bfd2eddd712c322608c2d9d
Alan Garrido <alangarrido44@gmail.com>
2023-11-02 03:28:30 -0300

    Avanzo hasta clase 6

---
+++ Alan/Javascript2023/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,15 @@
+var nombre = "Jose";
+var apellido = "Montes";
+var nombreCompleto = nombre + " " + apellido; //primera concatenacion
+console.log(nombreCompleto);
+var nombreCompleto2 = "Ariel" + " " + "Betancud"; //segunda concatenacion
+console.log(nombreCompleto2);
+var juntos = nombre + 219; //lee de izquierda a derecha siguiendo la cadena lee el numero como str
+console.log(juntos);
+juntos = nombre + (78 + 17); //aqui suma por los parentesis
+console.log(juntos);
+juntos = 78 + 17 + nombre; //primero sumo los numeros, despues sumo el nombre
+console.log(juntos);
+
+nombre += apellido; //tercera concatenacion usando el operador simplificado
+console.log(nombre);
