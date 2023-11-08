7fe69d69133a2e9e5d4278dcaf056af0536d441a
Hernan <hernanraso@gmail.com>
2023-11-07 21:53:51 -0300

    Agregando la leccion 3 y 4 de JavaScript que pude recuperar

---
+++ Hernan Raso/Js/Leccion 3/01-01-holamundo.js
@@ -0,0 +1,5 @@
+
+var nombre = "Hernan";
+nombre = "Hernan\nNelson\n"
+var apellido = "Raso"
+console.log(nombre + apellido);
---
+++ Hernan Raso/Js/Leccion 3/02-01-TiposDatos.js
@@ -0,0 +1,19 @@
+// Tipos de datos en JavaScript
+/*
+La sinstaxis en lo que es comentarios es muy similar a la de java realmente diriamos que es identica
+*/
+
+var nombre = "Hernan"; //Tipo str
+console.log(nombre);
+
+var numero = 3000; //tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Hernan",
+    apellido: "Raso",
+    telefono: "2343554234"
+
+}
+
+console.log(objeto);
