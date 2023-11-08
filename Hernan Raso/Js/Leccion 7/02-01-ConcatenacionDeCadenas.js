e8632024b36a53f8667a76235bba3a979b9501e2
Hernan <hernanraso@gmail.com>
2023-11-07 23:25:32 -0300

    Agregando la Leccion 7 de JavaScript

---
+++ Hernan Raso/Js/Leccion 7/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,33 @@
+var nombre = 'Jose';
+var apellido = ' Montes';
+var nombreCompleto = nombre+ ' ' +apellido; //Primera concatenacion
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Ariel' +' '+'Betancud';
+console.log(nombreCompleto2); // Segunda concatenacion
+var juntos = nombre + 219; //Lee de izq a der siguiendo la cadena lee el numero como str
+console.log(juntos);
+juntos = nombre + (78 + 17) //Aqui se puede diferencia a traves de los parentesis
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido; //Tercera Concatenacion usando el operador simplificado
+console.log(nombre);
+
+//Hoy ya no se una var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres; una constante no puede ser modificada"
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+let $1num = 31; //No utilizar numeros para iniciar el nombre de una variable
+let $break = "rompe"; //No utilizar palabras reservadas para variables
+
+
---
+++ Hernan Raso/Js/Leccion 7/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,20 @@
+//Ejercicio para encontrar numeros pares e impares
+let parInpar = 10;
+if(parInpar % 2 == 0){
+    console.log("Es un numero Par");
+}
+
+else{
+    console.log("Es un numero IMPAR");
+
+}
+
+//Ejercicio: es mayor de edad
+
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log('Usted es una persona adulta');
+}
+else{
+    console.log('Usted es una persona menor de edad');
+}
